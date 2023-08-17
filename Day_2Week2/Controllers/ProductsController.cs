96324e3d1b1ceb5df1f65b66b35be51f312a0b2a
thakkarjanvi <janvithakkar18122001@gmail.com>
2023-08-17 17:05:32 +0530

    Week-2 Day-2 Tasks

---
+++ Day_2Week2/Controllers/ProductsController.cs
@@ -0,0 +1,106 @@
+﻿using Day_2Week2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day_2Week2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private static List<Product> _productList = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product 1", Price = 10.99m },
+            new Product { Id = 2, Name = "Product 2", Price = 20.49m },
+            new Product { Id = 3, Name = "Product 3", Price = 15.75m }
+        };
+
+        public IActionResult Index()
+        {
+            return View(_productList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = _productList.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                // Simulate database insert
+                product.Id = _productList.Count + 1;
+                _productList.Add(product);
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var product = _productList.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var existingProduct = _productList.FirstOrDefault(p => p.Id == id);
+                if (existingProduct != null)
+                {
+                    existingProduct.Name = product.Name;
+                    existingProduct.Price = product.Price;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = _productList.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var product = _productList.FirstOrDefault(p => p.Id == id);
+            if (product != null)
+            {
+                _productList.Remove(product);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Day_2Week2/Models/ProductDetailsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Day_2Week2.Models
+{
+    public class ProductDetailsViewModel
+    {
+        public Product Product { get; set; }
+        public string DisplayMessage { get; set; }
+    }
+}
